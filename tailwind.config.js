c7a4fba1bfa6a02b30945d7a988f7cf88932c2a7
Damir Dimic <dimicdamir@gmail.com>
2021-06-03 01:48:06 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+
+     './src/**/*.html',
+
+     './src/**/*.js',
+     
+     './src/**/*.css',
+
+   ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
